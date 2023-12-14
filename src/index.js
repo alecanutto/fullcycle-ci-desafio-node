cf7bfd403ea37edfbdb5bd5dade7386a3f36338c
Alessandra Canuto <ale.canutto@gmail.com>
2023-12-14 14:04:28 -0300

    feat: add other math functions

--- src/index.js
+++ src/index.js
@@ -1,10 +1,33 @@
 const sum = (a, b) => a + b;
 
+const sub = (a, b) => a - b;
+
+const times = (a, b) => a * b;
+
+const div = (a, b) => a / b;
+
+const pow = (a, b) => a ** b;
+
+const sqrt = (a) => Math.sqrt(a);
+
+const max = (a, b) => Math.max(a, b);
+
+const min = (a, b) => Math.min(a, b);
+
+const round = (a, b) => Math.round(a, b);
+
 (() => {
-  const a = 1;
-  const b = 2;
-  const c = sum(a, b);
-  console.log(`${a} + ${b} = ${c}`);
+  console.log('Some math functions');
 })();
 
-module.exports = sum;
+module.exports = {
+  sum,
+  sub,
+  times,
+  div,
+  pow,
+  sqrt,
+  max,
+  min,
+  round,
+};
